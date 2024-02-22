1beb2d8d1fa72b5910f25804e80c454ef1277491
Shubhamsolanki1 <157217149+Shubhamsolanki1@users.noreply.github.com>
2024-02-22 13:56:37 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,27 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+a = "I"
+I = 1
+print(a)
+
+b = "IV"
+IV = 4
+print(b)
+
+c = "X"
+X = 10
+print(c)
+
+d = "L"
+L = 50
+print(d)
+
+e = "C"
+C = 100
+print(e)
+
+f = "D"
+D = 500
+print(f)
+
+g = "M" 
+M = 1000
+print(g)
